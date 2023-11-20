973bd069bec927a841158ba0f0320ada4e2f5e20
Nahuel Navarro <104699212+NahuelNavarro@users.noreply.github.com>
2023-11-20 20:01:36 -0300

    asd

--- src/components/FinalizarCompra/FinalizarCompraFinal.js
+++ src/components/FinalizarCompra/FinalizarCompraFinal.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import {addDoc, collection, getFirestore} from "firebase/firestore";
+import { useState } from 'react';
+
 
 export default function FinalizarCompraFinal() {
+
+  const [idpedido, setIdpedido] = useState("")
+
+const order={
+  buyer:{
+    name:"pablo"
+  }
+}
+
+  const handleClick = () =>{
+    const db = getFirestore();  
+    const ordersCollection = collection(db, "orders");
+    addDoc(ordersCollection, order)
+    .then(doc => {
+      setIdpedido(doc.id)
+     
+    })
+  }
+
   return (
-    <div>Gracias por su compra
+
+    
+    <div>
+      Gracias por su compra
+      <button onClick={handleClick}>generar orden de compra</button>
+      <div>{idpedido}</div>
+     
         
     </div>
+
     
   )
 }
